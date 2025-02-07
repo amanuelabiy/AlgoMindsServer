11d8904f5ab3f5c99306d9f89ac8e1036a7ab057
Amanuel Abiy <amanuelabiy41@gmail.com>
2025-02-06 23:12:48 -0500

    Added Server

---
+++ server/algo-rivals/src/main/java/com/algo_rivals/algo_rivals/AlgoRivalsApplication.java
@@ -0,0 +1,13 @@
+package com.algo_rivals.algo_rivals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoRivalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoRivalsApplication.class, args);
+	}
+
+}
---
+++ server/algo-rivals/src/test/java/com/algo_rivals/algo_rivals/AlgoRivalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algo_rivals.algo_rivals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoRivalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
