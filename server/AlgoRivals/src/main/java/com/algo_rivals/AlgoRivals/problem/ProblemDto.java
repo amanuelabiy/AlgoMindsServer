7f360f37bfae34928362843df72a7656f60c9d82
Amanuel Abiy <amanuelabiy41@gmail.com>
2025-02-11 19:26:13 -0500

    Added Problem Dto

---
+++ server/AlgoRivals/src/main/java/com/algo_rivals/AlgoRivals/problem/ProblemDto.java
@@ -0,0 +1,4 @@
+package com.algo_rivals.AlgoRivals.problem;
+
+public class ProblemDto {
+}
--- server/AlgoRivals/src/main/java/com/algo_rivals/AlgoRivals/problem/ProblemRepository.java
+++ server/AlgoRivals/src/main/java/com/algo_rivals/AlgoRivals/problem/ProblemRepository.java
@@ -3,7 +3,11 @@ package com.algo_rivals.AlgoRivals.problem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProblemRepository extends JpaRepository<Problem, Integer> {
+
+
 
 }
