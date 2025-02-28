e05b68b35b3bcfe5ad9c0c03676fd1a0736b7180
Markose Tsegaye <103059891+MarkoseTsegaye@users.noreply.github.com>
2025-02-27 22:33:36 -0500

    Removed axios, updated to use fetch.

--- server/src/modules/judge0/judge0.service.ts
+++ server/src/modules/judge0/judge0.service.ts
@@ -1,29 +1,36 @@
-import axios, { AxiosRequestHeaders } from "axios";
 import { config } from "../../config/app.config";
 
 export class Judge0Service {
-   async submitCode(sourceCode: string, languageId: number, stdin?: string) {
-    try {
-      const response = await axios.post(`${config.JUDGE0_API_BASE_URL}/submissions`, {
-        source_code: sourceCode,  // ✅ Send as raw text (NOT Base64)
-        language_id: languageId,
-        stdin: stdin ? Buffer.from(stdin).toString("base64") : null,  // Keep Base64 for stdin
-        expected_output: null,
-        redirect_stderr_to_stdout: true,
-      }, { headers: config.JUDGE0_HEADERS  });
+  async submitCode(sourceCode: string, languageId: number, stdin?: string) {
+    const response = await fetch(
+      `${config.JUDGE0_API_BASE_URL}/submissions`,
+      {
+        method: "POST",
+        headers: config.JUDGE0_HEADERS,
+        body: JSON.stringify({
+          source_code: sourceCode, // ✅ Send as raw text (NOT Base64)
+          language_id: languageId,
+          stdin: stdin ? Buffer.from(stdin).toString("base64") : null, // Keep Base64 for stdin
+          expected_output: null,
+          redirect_stderr_to_stdout: true,
+        }),
+      },
+      
+    );
 
-      return response.data.token;
-    } catch (error) {
-      throw new Error(`Error submitting code: ${error}`);
-    }
+    const result = await response.json();
+    return result.token;
   }
 
-   async getSubmissionResult(token: string) {
+  async getSubmissionResult(token: string) {
     try {
-      const response = await axios.get(`${config.JUDGE0_API_BASE_URL}/submissions/${token}`, {
-        headers:  config.JUDGE0_HEADERS ,
-      });
-      return response.data;
+      const response = await fetch(
+        `${config.JUDGE0_API_BASE_URL}/submissions/${token}`,
+        {
+          headers: config.JUDGE0_HEADERS,
+        }
+      );
+      return response.json();
     } catch (error) {
       throw new Error(`Error fetching submission result: ${error}`);
     }
