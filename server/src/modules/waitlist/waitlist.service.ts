1a35af301e12d0d32f260740f4e0c975bd53c527
Amanuel Abiy <amanuelabiy41@gmail.com>
2025-02-28 21:51:15 -0500

    Email sending works on adding to waitlist

--- server/src/modules/waitlist/waitlist.service.ts
+++ server/src/modules/waitlist/waitlist.service.ts
@@ -1,5 +1,8 @@
 import { ErrorCode } from "../../common/enums/error-code.enum";
 import { BadRequestException } from "../../common/utils/catch-errors";
+import { config } from "../../config/app.config";
+import { sendEmail } from "../../mailers/mailer";
+import { waitListTemplate } from "../../mailers/templates/templates";
 import { WaitListRepository } from "./waitlist.repository";
 
 export class WaitListService {
@@ -18,6 +21,12 @@ export class WaitListService {
         ErrorCode.WAITLIST_EMAIL_ALREADY_EXISTS
       );
     }
+
+    // Send email to user
+    await sendEmail({
+      to: email,
+      ...waitListTemplate(config.APP_ORIGIN),
+    });
 
     return await this.waitListRepository.createEntry(email);
   }
