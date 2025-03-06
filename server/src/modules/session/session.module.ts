412cbe5d198ff4691a982e8b7a6c46a9c3e85db0
Amanuel Abiy <amanuelabiy41@gmail.com>
2025-03-06 18:23:24 -0500

    Included user Preferences when getting the user associated with the session

--- server/src/modules/session/session.module.ts
+++ server/src/modules/session/session.module.ts
@@ -1,9 +1,10 @@
+import { userService } from "../user/user.module";
 import { SessionController } from "./session.controller";
 import { SessionRepository } from "./session.repository";
 import { SessionService } from "./session.service";
 
 const sessionRepository = new SessionRepository();
-const sessionService = new SessionService(sessionRepository);
+const sessionService = new SessionService(sessionRepository, userService);
 const sessionController = new SessionController(sessionService);
 
 export { sessionService, sessionController };
